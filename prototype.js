6b687446385c34ea8674a2ba98f69e0faa649ee7
Steve Madrigal <Steve Madrigal>
2016-11-10 05:36:51 -0500

    Added bootstrap to make css easier

---
+++ prototype.js
@@ -0,0 +1,28 @@
+function openPage(event, tabName) {
+    var i, tabcontent, tablinks;
+	
+    tabcontent = document.getElementsByClassName("content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+	
+    tablinks = document.getElementsByClassName("tabLink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+	
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+function openNav(){
+	document.getElementById("mainNav").style.width = "250px";
+	document.getElementById("container").style.marginLeft = "250px";
+}
+
+function closeNav(){
+	document.getElementById("mainNav").style.width = "0";
+	document.getElementById("mainPage").style.marginLeft = "0";
+}
+
+document.getElementById("defaultOpen").click();
